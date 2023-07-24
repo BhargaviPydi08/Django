0b64cdfa0cfc76e580275378ecde92b82e1fc589
BhargaviPydi <lakshmibhargavipydi@gmail.com>
2023-07-24 13:17:24 +0530

    Final Commit

--- crmwebsite/urls.py
+++ crmwebsite/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     #path('login/', views.login_user,name='Login'),
     path('logout/', views.logout_user,name='Logout'),
     path('register/', views.register_user,name='Register'),
+    path('record/<int:pk>', views.customer_record,name='record'),
+    path('delete_record/<int:pk>', views.delete_record,name='delete_record'),
+    path('add_record/', views.add_record,name='add_record'),
+    path('update_record/<int:pk>', views.update_record,name='update_record'),
+
+
 
 
    
